95116570ebddd4fd1ed4034bdee7e5c9d53fc21c
ajarias01 <ariasalan634@gmail.com>
2026-04-26 17:42:20 -0500

    add frames

--- 2dShapesGroup/2dShapesGroup/obj/Release/net10.0-windows/2dShapesGroup.AssemblyInfo.cs
+++ 2dShapesGroup/2dShapesGroup/obj/Release/net10.0-windows/2dShapesGroup.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("2dShapesGroup")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cf080f496efe86fa48fe0b5e475b4ab0f41d06a")]
 [assembly: System.Reflection.AssemblyProductAttribute("2dShapesGroup")]
